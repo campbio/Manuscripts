068016496e9ce51f99f82fa07e3ab70eb857835e
zhewa <zhe@bu.edu>
2021-02-28 19:21:24 -0500

    add figureS2

--- Celda/R/FigureS3.R
+++ Celda/R/FigureS3.R
@@ -1,132 +1,176 @@
-# Figure S3: Seurat and scran clusterings
+# Figure S3: Marker gene expressions in plasma cell and B cells
 
 library(celda)
-library(Seurat)
-library(scran)
-library(scater)
+library(SingleCellExperiment)
+library(ggplot2)
+library(data.table)
 
 sce <- readRDS("../Data/sce.rds")
 
-K <- 20
-L <- 80
-
-# Seurat
-pbmc4kseurat <- CreateSeuratObject(counts = decontXcounts(sce),
-    project = "pbmc4kdec", min.cells = 3, min.features = 200)
-dim(pbmc4kseurat)
-
-pbmc4kseurat <- NormalizeData(pbmc4kseurat)
-pbmc4kseurat <- FindVariableFeatures(pbmc4kseurat)
-pbmc4kseurat <- ScaleData(pbmc4kseurat)
-pbmc4kseurat <- RunPCA(pbmc4kseurat)
-#ElbowPlot(pbmc4kseurat, ndims = 50)
-pbmc4kseurat <- FindNeighbors(pbmc4kseurat, dims = seq(22))
-pbmc4kseurat <- FindClusters(pbmc4kseurat)
-pbmc4kseurat <- RunUMAP(pbmc4kseurat, dims = seq(22),
-    n.neighbors = 10, min.dist = 0.5)
-
-
-g1 <- DimPlot(pbmc4kseurat, reduction = "umap", label = TRUE, pt.size = 0.7) +
-    theme(legend.position = "none")
-
-markers <- c("CD3D", "CD3E", "CD3G", "CD8A", "CD8B", "GZMA", "GZMK", #CD8+ T
-    "MS4A1", "CD19", #B
-    "CD4", "CCR7", "CD27", "SELL", # Naive CD4+ T
-    "IL7R", "S100A4", # Memory CD4+ T
-    "NKG7", "GZMB", "GNLY", # NK
-    "FCGR3A", # FCGR3A+ mono
-    "CD14", "LYZ", "S100A9", # CD14+ mono
-    "MKI67", "CENPF", "CENPM", # Activated T
-    "FCER1A", "HLA-DQA1", "HLA-DPB1", "HLA-DRB1", "CST3", # DC
-    "CLEC4C", "PLAC8", "IRF7", "IRF8", # pDC
-    "PPBP", "ITGA2B", "CXCR4", # Mk
-    "CD34", # CD34+
-    "MT-CO1", "MT-CO2", "MT-CO3", "IGKC",
-    "IGHG1", "IGHG2", "IGHG3", # pc
-    "IGLC2", "IGLC3")
-
-len <- length(markers)
-grids <- len / 9
-if (len %% 9 != 0) {
-    grids <- as.integer(grids + 1)
-}
-for (i in seq(grids)) {
-    nums <- seq(((i - 1) * 9) + 1, min(i * 9, len))
-    g <- FeaturePlot(pbmc4kseurat, features = markers[nums],
-        reduction = "umap", order = TRUE, pt.size = 0.1)
-    print(g)
+.themePublication <- function(base_size = 12,
+    base_family = "sans") {
+    (ggthemes::theme_foundation(base_size = base_size,
+        base_family = base_family) +
+            ggplot2::theme(plot.title = ggplot2::element_text(
+                face = "bold",
+                size = ggplot2::rel(1),
+                hjust = 0.5),
+                text = ggplot2::element_text(),
+                panel.background = ggplot2::element_rect(color = NA),
+                plot.background = ggplot2::element_rect(color = NA),
+                panel.border = ggplot2::element_rect(color = NA),
+                axis.title = ggplot2::element_text(
+                    face = "bold",
+                    size = ggplot2::rel(1)),
+                axis.title.y = ggplot2::element_text(angle = 90,
+                    vjust = 2),
+                axis.title.x = ggplot2::element_text(vjust = -0.2),
+                axis.text = ggplot2::element_text(),
+                axis.line = ggplot2::element_line(color = "black"),
+                axis.ticks = ggplot2::element_line(),
+                panel.grid.major = ggplot2::element_line(color = "#f0f0f0"),
+                panel.grid.minor = ggplot2::element_blank(),
+                legend.key = ggplot2::element_rect(color = NA),
+                legend.position = "right",
+                legend.direction = "vertical",
+                legend.key.size = ggplot2::unit(0.5, "cm"),
+                legend.margin = ggplot2::margin(0),
+                legend.title = ggplot2::element_text(face = "bold"),
+                plot.margin = ggplot2::unit(c(10, 5, 5, 5), "mm"),
+                strip.background = ggplot2::element_rect(
+                    color = "#f0f0f0", fill = "#f0f0f0"),
+                strip.text = ggplot2::element_text(face = "bold")
+            ))
 }
 
-# scran
-set.seed(12345)
-ae <- altExp(sce)
-assay(ae, "counts") <- NULL
+de1 <- differentialExpression(sce,
+    useAssay = "decontXcounts",
+    c1 = c(15, 16, 17, 18, 19, 20))
 
-ae <- logNormCounts(ae, assay.type = "decontXcounts")
-ae <- runPCA(ae, ntop = 2000) # no scaling
+de1[Gene %in% c("CD3D", "CD3E", "CD3G"), ]
+#    Gene Pvalue  Log2_FC FDR
+# 1: CD3D      0 6.200828   0
+# 2: CD3E      0 5.565626   0
+# 3: CD3G      0 4.046326   0
 
-output <- getClusteredPCs(reducedDim(ae, "PCA"))
-npcs <- metadata(output)$chosen
-reducedDim(ae, "PCAsub") <- reducedDim(ae, "PCA")[, seq(npcs), drop=FALSE]
-# npcs # 30
+de2 <- differentialExpression(sce,
+    useAssay = "decontXcounts",
+    c1 = c(17, 18, 19),
+    c2 = c(15, 16, 20))
 
-graph <- buildSNNGraph(ae, use.dimred = "PCAsub")
-cluster <- igraph::cluster_walktrap(graph)$membership
+de2[Gene %in% c("CD8A", "CD8B"), ]
+#    Gene        Pvalue  Log2_FC           FDR
+# 1: CD8B 5.910581e-176 3.287508 4.978778e-172
+# 2: CD8A 7.534401e-169 3.024783 5.077282e-165
 
-colLabels(ae) <- factor(cluster)
-table(colLabels(ae))
-length(unique(colLabels(ae)))
-# 24
+de3 <- differentialExpression(sce,
+    useAssay = "decontXcounts",
+    c1 = c(17),
+    c2 = c(18, 19))
 
-ae <- runUMAP(ae, dimred = "PCAsub", n_neighbors = 5)
+de3[Gene %in% c("CCR7"), ]
+#    Gene       Pvalue  Log2_FC          FDR
+# 1: CCR7 3.415604e-88 3.279663 3.110415e-85
 
-g2 <- plotUMAP(ae, colour_by = "label", text_by = "label", point_alpha = 1) +
-    scale_color_manual(values = distinctColors(length(unique(colLabels(ae))))) +
-    theme(legend.position = "none")
+de4 <- differentialExpression(sce,
+    useAssay = "decontXcounts",
+    c1 = c(18),
+    c2 = c(17, 19))
 
-pdf("../Figures/FigureS3.pdf")
-print(g1)
-print(g2)
+de4[Gene %in% c("GNLY", "KLRG1", "GZMA", "GZMH"), ]
+#     Gene        Pvalue  Log2_FC           FDR
+# 1:  GZMA 5.602638e-104 4.657639 3.775506e-100
+# 2:  GZMH  5.513208e-88 3.599870  2.322025e-84
+# 3: KLRG1  1.439579e-48 3.012203  1.054460e-45
+# 4:  GNLY  2.251357e-34 2.259016  8.719222e-32
+
+de5 <- differentialExpression(sce,
+    useAssay = "decontXcounts",
+    c1 = c(1),
+    c2 = c(2, 3, 4))
+
+de5[Gene %in% c("IGHG1", "IGHG3", "IGLC2"), ]
+#     Gene       Pvalue  Log2_FC          FDR
+# 1: IGHG1 8.906256e-11 1.583893 2.326259e-08
+# 2: IGHG3 1.030868e-10 1.483946 2.651455e-08
+# 3: IGLC2 5.638171e-04 3.802954 1.860652e-02
+
+plasmac <- which(celdaClusters(sce) == 1)
+# [1] 2904
+
+sum(counts(sce)[, plasmac])
+# [1] 48443
+
+bcells <- which(celdaClusters(sce) %in% c(2, 3, 4))
+
+pcgenes <- c("CD79A", "CD79B", "CD19", "IGKC", "IGHG1", "IGHG3", "IGLC2",
+    "IGLC3")
+# plasma cell UMI counts
+counts(sce)[pcgenes, plasmac]
+# CD79A CD79B  CD19  IGKC IGHG1 IGHG3 IGLC2 IGLC3
+#    10     8     2     2  1982  1409  8029  1480
+
+# mean B-cell UMI counts
+apply(counts(sce)[pcgenes, bcells], MARGIN = 1, mean)
+#     CD79A      CD79B       CD19       IGKC      IGHG1      IGHG3      IGLC2
+# 5.9934747  5.5644372  0.5791191 23.3964111  0.6084829  0.2805873  7.0864600
+#     IGLC3
+# 6.1011419
+
+# median B-cell UMI counts
+apply(counts(sce)[pcgenes, bcells], MARGIN = 1, median)
+# CD79A CD79B  CD19  IGKC IGHG1 IGHG3 IGLC2 IGLC3
+#     5     5     0    21     0     0     1     0
+
+sort(counts(sce)[, plasmac], decreasing = TRUE)[1:20]
+# IGLC2  IGHG1  IGLC3  IGHG3 MALAT1    B2M  RPL41  RPS18  RPLP1   RPS2  RPL13
+#  8029   1982   1480   1409    580    399    359    339    339    269    259
+# HLA-B TMSB10  RPL10  RPL21   RPS6  RPS19 RPL18A RPL13A   RPS3
+#   239    234    232    220    207    205    199    193    191
+
+# fraction of "IGHG1", "IGHG3", "IGLC2", "IGLC3" counts in plasma cell
+sum(counts(sce)[c("IGHG1", "IGHG3", "IGLC2", "IGLC3"), plasmac]) /
+    sum(counts(sce)[, plasmac])
+#[1] 0.2662923
+
+
+gdt <- data.table(fts = c(pcgenes, "Total counts"),
+    pcell = c(counts(sce)[pcgenes, plasmac], sum(counts(sce)[, plasmac])),
+    bcellavg = c(apply(counts(sce)[pcgenes, bcells], MARGIN = 1, mean),
+        mean(colSums(counts(sce)[, bcells]))))
+gdt[, fts := factor(fts, levels = c(pcgenes, "Total counts"))]
+gdt[, pcelllog2 := log10(pcell + 1)]
+gdt[, bcellavglog2 := log10(bcellavg + 1)]
+gdt[, log2fc := log2(pcell / bcellavg)]
+gdt[, ytext := max(.SD), .SDcols = c("pcelllog2", "bcellavglog2"),
+    by = seq(nrow(gdt))]
+gdtm <- melt(gdt, id.vars = c("fts", "log2fc"),
+    measure.vars = c("pcelllog2", "bcellavglog2"))
+
+
+# figure S3
+g <- ggplot() +
+    geom_bar(data = gdtm, aes(x = fts, y = value, fill = variable),
+        stat = "identity", position = position_dodge()) +
+    geom_text(data = gdt,
+        aes(x = fts, y = ytext, label = round(log2fc, digits = 2)),
+        vjust = -0.3, size = 5) +
+    ylab("Counts + 1") +
+    xlab(NULL) +
+    scale_fill_discrete(labels = c("Plasma cell", "B-cell average")) +
+    scale_y_continuous(breaks = c(0, 1, 2, 3, 4, 5),
+        labels = c(1, 10, 100, "1,000", "10,000", "100,000"),
+        limits = c(NA, 5)) +
+    .themePublication() +
+    theme(legend.title = element_blank(),
+        legend.text = element_text(size = 14),
+        axis.text = element_text(size = 14),
+        axis.title = element_text(size = 16),
+        axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
+
+pdf("../Figures/FigureS3.pdf", width = 10)
+print(g)
 dev.off()
-
-
-markers <- c("CD3D", "CD3E", "CD3G", "CD8A", "CD8B", "GZMA", "GZMK", #CD8+ T
-    "MS4A1", "CD19", #B
-    "CD4", "CCR7", "CD27", "SELL", # Naive CD4+ T
-    "IL7R", "S100A4", # Memory CD4+ T
-    "NKG7", "GZMB", "GNLY", # NK
-    "FCGR3A", # FCGR3A+ mono
-    "CD14", "LYZ", "S100A9", # CD14+ mono
-    "MKI67", "CENPF", "CENPM", # Activated T
-    "FCER1A", "HLA-DQA1", "HLA-DPB1", "HLA-DRB1", "CST3", # DC
-    "CLEC4C", "PLAC8", "IRF7", "IRF8", # pDC
-    "PPBP", "ITGA2B", "CXCR4", # Mk
-    "CD34", # CD34+
-    "MT-CO1", "MT-CO2", "MT-CO3", "IGKC",
-    "IGHG1", "IGHG2", "IGHG3", # pc
-    "IGLC2", "IGLC3")
-
-
-len <- length(markers)
-grids <- len / 9
-if (len %% 9 != 0) {
-    grids <- as.integer(grids + 1)
-}
-
-altExp(sce) <- ae
-for (i in seq(grids)) {
-    nums <- seq(((i - 1) * 9) + 1, min(i * 9, len))
-    g <- plotDimReduceFeature(sce,
-        altExpName = "featureSubset",
-        useAssay = "decontXcounts",
-        reducedDimName = "UMAP",
-        features = markers[nums],
-        decreasing = FALSE,
-        size = 0.1)
-    print(g)
-}
-
-
 
 
 
--- Celda/R/FigureS4.R
+++ Celda/R/FigureS4.R
@@ -1,158 +1,132 @@
+# Figure S4: Seurat and scran clusterings
 
 library(celda)
 library(Seurat)
-library(SingleCellExperiment)
-library(ggplot2)
-library(data.table)
-library(patchwork)
-library(ComplexHeatmap)
-library(gridExtra)
+library(scran)
 library(scater)
 
 sce <- readRDS("../Data/sce.rds")
 
+K <- 20
 L <- 80
 
-colorLow = "blue4"
-colorMid = "grey90"
-colorHigh = "firebrick1"
-limits = c(-2, 2)
-midpoint = 0
+# Seurat
+pbmc4kseurat <- CreateSeuratObject(counts = decontXcounts(sce),
+    project = "pbmc4kdec", min.cells = 3, min.features = 200)
+dim(pbmc4kseurat)
 
-seuratobj <- CreateSeuratObject(counts = decontXcounts(sce),
-    project = "pca", min.cells = 3, min.features = 200)
-
-seuratobj <- NormalizeData(seuratobj, verbose = TRUE)
-seuratobj <- FindVariableFeatures(seuratobj, nfeatures = 2000,
-    verbose = TRUE)
-
-seuratobj <- ScaleData(seuratobj, verbose = TRUE)
-seuratobj <- RunPCA(seuratobj, npcs = L, verbose = TRUE)
-
-
-pchm <- function(seuratobj,
-    dim,
-    ncells = 100,
-    nfeatures = 30,
-    balanced = TRUE,
-    annotation_height = unit(1.5, "mm"),
-    fontsize = 12,
-    showHeatmapLegend = FALSE,
-    showAnnoLegend = FALSE,
-    showColumnTitle = FALSE,
-    col = circlize::colorRamp2(c(-2, 0, 2),
-        c("#1E90FF", "#FFFFFF", "#CD2626")),
-    ...) {
+pbmc4kseurat <- NormalizeData(pbmc4kseurat)
+pbmc4kseurat <- FindVariableFeatures(pbmc4kseurat)
+pbmc4kseurat <- ScaleData(pbmc4kseurat)
+pbmc4kseurat <- RunPCA(pbmc4kseurat)
+#ElbowPlot(pbmc4kseurat, ndims = 50)
+pbmc4kseurat <- FindNeighbors(pbmc4kseurat, dims = seq(22))
+pbmc4kseurat <- FindClusters(pbmc4kseurat)
+pbmc4kseurat <- RunUMAP(pbmc4kseurat, dims = seq(22),
+    n.neighbors = 10, min.dist = 0.5)
 
 
-    cells <- TopCells(
-        object = seuratobj[["pca"]],
-        dim = dim,
-        ncells = ncells,
-        balanced = balanced)
+g1 <- DimPlot(pbmc4kseurat, reduction = "umap", label = TRUE, pt.size = 0.7) +
+    theme(legend.position = "none")
 
-    if (balanced) {
-        cells$negative <- rev(x = cells$negative)
-    }
-    cells <- unlist(x = unname(obj = cells))
+markers <- c("CD3D", "CD3E", "CD3G", "CD8A", "CD8B", "GZMA", "GZMK", #CD8+ T
+    "MS4A1", "CD19", #B
+    "CD4", "CCR7", "CD27", "SELL", # Naive CD4+ T
+    "IL7R", "S100A4", # Memory CD4+ T
+    "NKG7", "GZMB", "GNLY", # NK
+    "FCGR3A", # FCGR3A+ mono
+    "CD14", "LYZ", "S100A9", # CD14+ mono
+    "MKI67", "CENPF", "CENPM", # Activated T
+    "FCER1A", "HLA-DQA1", "HLA-DPB1", "HLA-DRB1", "CST3", # DC
+    "CLEC4C", "PLAC8", "IRF7", "IRF8", # pDC
+    "PPBP", "ITGA2B", "CXCR4", # Mk
+    "CD34", # CD34+
+    "MT-CO1", "MT-CO2", "MT-CO3", "IGKC",
+    "IGHG1", "IGHG2", "IGHG3", # pc
+    "IGLC2", "IGLC3")
 
-    tfea <- TopFeatures(seuratobj[["pca"]],
-        dim = dim,
-        nfeatures = nfeatures,
-        balanced = balanced)
-    tfea <- unique(c(tfea$negative, rev(tfea$positive)))
-
-    datascale <- seuratobj@assays$RNA@scale.data
-
-    dts <- datascale[tfea, cells]
-
-    cols <- distinctColors(20)
-    dtannot <- data.table(cellid = cells,
-        celda_cluster = celdaClusters(sce[, cells]))
-    dtannot[, color := cols[celda_cluster]]
-    colmap <- c(dtannot$color)
-    names(colmap) <- dtannot$celda_cluster
-
-    hm1 <- Heatmap(matrix = dts,
-        col = col,
-        cluster_rows = FALSE,
-        cluster_columns = FALSE,
-        show_column_names = FALSE,
-        show_heatmap_legend = showHeatmapLegend,
-        row_names_gp = gpar(fontsize = fontsize),
-        top_annotation = HeatmapAnnotation(cellcluster = dtannot$celda_cluster,
-            col = list(cellcluster = colmap),
-            show_legend = showAnnoLegend,
-            show_annotation_name = FALSE,
-            simple_anno_size = annotation_height),
-        column_title = ifelse(showColumnTitle,
-            paste0("PC ", dim), ""),
-        ...)
-    return(hm1)
+len <- length(markers)
+grids <- len / 9
+if (len %% 9 != 0) {
+    grids <- as.integer(grids + 1)
+}
+for (i in seq(grids)) {
+    nums <- seq(((i - 1) * 9) + 1, min(i * 9, len))
+    g <- FeaturePlot(pbmc4kseurat, features = markers[nums],
+        reduction = "umap", order = TRUE, pt.size = 0.1)
+    print(g)
 }
 
+# scran
+set.seed(12345)
+ae <- altExp(sce)
+assay(ae, "counts") <- NULL
 
-pchmdims <- function(seuratobj,
-    dims,
-    nrow,
-    ncol,
-    ncells = 100,
-    nfeatures = 30,
-    balanced = TRUE,
-    annotation_height = unit(1.5, "mm"),
-    fontsize = 12,
-    gap = unit(2, "mm"),
-    padding = unit(rep(5.5, 4), "points"),
-    showColumnTitle = FALSE,
-    ...) {
+ae <- logNormCounts(ae, assay.type = "decontXcounts")
+ae <- runPCA(ae, ntop = 2000) # no scaling
 
-    pchm <- lapply(X = dims,
-        FUN = pchm,
-        seuratobj = seuratobj,
-        #dim = dims,
-        ncells = ncells,
-        nfeatures = nfeatures,
-        balanced = balanced,
-        annotation_height = annotation_height,
-        fontsize = fontsize,
-        showColumnTitle = showColumnTitle,
-        ... = ...)
+output <- getClusteredPCs(reducedDim(ae, "PCA"))
+npcs <- metadata(output)$chosen
+reducedDim(ae, "PCAsub") <- reducedDim(ae, "PCA")[, seq(npcs), drop=FALSE]
+# npcs # 30
 
-    glist <- lapply(pchm, function(x) {
-        grid.grabExpr(draw(x, gap = gap, padding = padding))
-    })
+graph <- buildSNNGraph(ae, use.dimred = "PCAsub")
+cluster <- igraph::cluster_walktrap(graph)$membership
 
-    g <- arrangeGrob(grobs = glist, nrow = nrow,
-        ncol = ncol)
-    return(g)
-}
+colLabels(ae) <- factor(cluster)
+table(colLabels(ae))
+length(unique(colLabels(ae)))
+# 24
 
-fontsize <- 5
-padlr <- 17
+ae <- runUMAP(ae, dimred = "PCAsub", n_neighbors = 5)
 
+g2 <- plotUMAP(ae, colour_by = "label", text_by = "label", point_alpha = 1) +
+    scale_color_manual(values = distinctColors(length(unique(colLabels(ae))))) +
+    theme(legend.position = "none")
 
-cols <- Seurat::PurpleAndYellow()
-col2 <- circlize::colorRamp2(seq(-2, 2, length.out = 50), cols)
-
-g1 <- pchmdims(seuratobj, seq(9), nrow = 3, ncol = 3,
-    #width = 10,
-    row_names_max_width = unit(7, "mm"),
-    fontsize = fontsize,
-    padding = unit(c(6, padlr, 6, padlr), "points"),
-    col = col2)
-
-pdf("../Figures/FigureS4.pdf")
-grid.draw(g1)
+pdf("../Figures/FigureS3.pdf")
+print(g1)
+print(g2)
 dev.off()
 
 
-# Percent variance explained
-sce2 <- SingleCellExperiment(assays = list(
-    counts = seuratobj@assays$RNA@scale.data))
-scaterpca <- scater::runPCA(sce2, ntop = 2000, exprs_values = "counts",
-    ncomponents = 80)
-red <- reducedDim(scaterpca, "PCA")
-str(red)
-attr(red, "percentVar")[1:9]
-# [1] 8.8633114 3.2198463 1.9634761 1.4881054 0.9732545 0.8353006 0.7893381
-# [8] 0.5367381 0.4450517
+markers <- c("CD3D", "CD3E", "CD3G", "CD8A", "CD8B", "GZMA", "GZMK", #CD8+ T
+    "MS4A1", "CD19", #B
+    "CD4", "CCR7", "CD27", "SELL", # Naive CD4+ T
+    "IL7R", "S100A4", # Memory CD4+ T
+    "NKG7", "GZMB", "GNLY", # NK
+    "FCGR3A", # FCGR3A+ mono
+    "CD14", "LYZ", "S100A9", # CD14+ mono
+    "MKI67", "CENPF", "CENPM", # Activated T
+    "FCER1A", "HLA-DQA1", "HLA-DPB1", "HLA-DRB1", "CST3", # DC
+    "CLEC4C", "PLAC8", "IRF7", "IRF8", # pDC
+    "PPBP", "ITGA2B", "CXCR4", # Mk
+    "CD34", # CD34+
+    "MT-CO1", "MT-CO2", "MT-CO3", "IGKC",
+    "IGHG1", "IGHG2", "IGHG3", # pc
+    "IGLC2", "IGLC3")
+
+
+len <- length(markers)
+grids <- len / 9
+if (len %% 9 != 0) {
+    grids <- as.integer(grids + 1)
+}
+
+altExp(sce) <- ae
+for (i in seq(grids)) {
+    nums <- seq(((i - 1) * 9) + 1, min(i * 9, len))
+    g <- plotDimReduceFeature(sce,
+        altExpName = "featureSubset",
+        useAssay = "decontXcounts",
+        reducedDimName = "UMAP",
+        features = markers[nums],
+        decreasing = FALSE,
+        size = 0.1)
+    print(g)
+}
+
+
+
+
+
