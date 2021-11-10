81fc118ac548da5dbdcda80edd81c3a8c09af8cd
Yusuke Koga <ykoga07@bu.edu>
2021-11-09 21:20:00 -0500

    Further edits for package installation

--- Hong_SCTK-QC/Scripts/SCTK_install.R
+++ Hong_SCTK-QC/Scripts/SCTK_install.R
@@ -4,6 +4,14 @@ if (!require("devtools", character.only = TRUE)) {
     install.packages("devtools", dependencies = TRUE)
 }
 
+if (!require("optparse", character.only = TRUE)) {
+    install.packages("optparse", dependencies = TRUE, repos = "http://cran.us.r-project.org")
+}
+
+if (!require("rmarkdown", character.only = TRUE)) {
+    install.packages("rmarkdown", dependencies = TRUE,repos = "http://cran.us.r-project.org")
+}
+
 if (!require("singleCellTK", character.only = TRUE)) {
     devtools::install_github("compbiomed/singleCellTK", dependencies = TRUE)
 }
